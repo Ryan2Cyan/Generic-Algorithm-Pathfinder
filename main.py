2ce5531427b927ebf163c567b4d48ef68aa3c647
Cyan244466666 <cammellryan@gmail.com>
2021-12-05 12:05:00 +0000

    added 'draw_grid' base function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-
+import pygame
+import sys
 
 # Init single bit (as a string):
 def gen_bit():
@@ -27,15 +28,47 @@ def extract_grid_data(filepath):
         f2.append(int(fp[x]))
     return f2
 
+# Init pygame window:
+def create_window(height, width):
+    BLACK = (0, 0, 0)
+    return pygame.display.set_mode((height, width)).fill(BLACK)
 
-all_chromosomes = []
+# Renders grid to screen:
+def draw_grid(WINDOW_WIDTH, WINDOW_HEIGHT, SCREEN, WHITE):
+    blockSize = 20  # Set the size of the grid block
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x + 160, y + 150, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
-# Generate chromosomes:
-for x in range(20):
-    all_chromosomes.append(gen_chromosome(5))
-    print(all_chromosomes[x])
 
-# # Read Grid Text File:
-# f = extract_grid_data("Lab8TerrainFile1.0.txt")
+def main():
+    pygame.init()
+    WINDOW_HEIGHT = 400
+    WINDOW_WIDTH = 400
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill((0, 0, 0))
 
-# Extract data and assign to correct variables:
+    while True:
+        draw_grid(60, 60, SCREEN, (200, 200, 200))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+    all_chromosomes = []
+
+    # Generate chromosomes:
+    for x in range(20):
+        all_chromosomes.append(gen_chromosome(5))
+        print(all_chromosomes[x])
+
+
+    # # Read Grid Text File:
+    # f = extract_grid_data("Lab8TerrainFile1.0.txt")
+
+    # Extract data and assign to correct variables:
+
+main()
