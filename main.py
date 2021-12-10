6bb707a51aa028e9b5819209a2069a53dcd7e70f
Cyan244466666 <cammellryan@gmail.com>
2021-12-10 10:11:54 +0000

    added functions to: i) find start pos. ii) find end pos. iii) take gene and output move array. iv) check for out of bounds. v) check if hit an obstacle.

--- main.py
+++ main.py
@@ -3,22 +3,34 @@ import random
 import pygame
 import sys
 
+"""
+Gene Movement Correlation:
+00 - Up
+01 - Right
+10 - Down
+11 - Left
+"""
+
 # Init single bit (as a string):
 def gen_bit():
     return str(random.randint(0, 1))
+
 # Init single gene (2 bits):
 def gen_gene():
     return gen_bit() + gen_bit()
+
 # Init chromosome:
 def gen_chromosome(gene_count):
     new_chromosome = []
     for x in range(gene_count):
         new_chromosome.append(gen_gene())
     return new_chromosome
+
 # Init pygame window:
 def create_window(height, width):
     BLACK = (0, 0, 0)
     return pygame.display.set_mode((height, width)).fill(BLACK)
+
 # Renders grid to screen:
 def draw_grid(WIDTH, HEIGHT, SCREEN, VALUES):
 
@@ -36,8 +48,9 @@ def draw_grid(WIDTH, HEIGHT, SCREEN, VALUES):
     # Render Grid Squares
     for row in range(HEIGHT):
         for column in range(WIDTH):
-            print(grid_color(VALUES[row][column]))
-            pygame.draw.rect(SCREEN, grid_color(VALUES[row][column]), rect_grid[row][column], 0)
+            pygame.draw.rect(SCREEN, grid_color(VALUES[row][column]), rect_grid[row][column], 0) # Grid Fill
+            pygame.draw.rect(SCREEN, (10, 30, 200), rect_grid[row][column], 1) # Grid Lines
+
 # Switch for Grid Square Color:
 def grid_color(grid_value):
     if grid_value == 0:
@@ -48,6 +61,7 @@ def grid_color(grid_value):
         return (10, 200, 30)
     elif grid_value == 3:
         return (10, 40, 200)
+
 # Extract Data from Grid File:
 def extract_grid_from_file(filepath):
     fp = open(filepath, "r").read().split(" ")
@@ -55,6 +69,7 @@ def extract_grid_from_file(filepath):
     for x in range(len(fp)):
         f2.append(int(fp[x]))
     return f2
+
 # Sorts Grid Values into a 2-D Array
 def sort_grid_array(WIDTH, HEIGHT, FILE_DATA_ARRAY):
     GRID_VALUES = []
@@ -69,8 +84,60 @@ def sort_grid_array(WIDTH, HEIGHT, FILE_DATA_ARRAY):
             i = i + 1
     return GRID_VALUES
 
+def find_start_point(GRID_ARRAY, HEIGHT, WIDTH):
+    starting_pos = []
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            if GRID_ARRAY[y][x] == 2:
+                starting_pos.append(y)
+                starting_pos.append(x)
+    return starting_pos
+
+def find_finish_point(GRID_ARRAY, HEIGHT, WIDTH):
+    finishing_pos = []
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            if GRID_ARRAY[y][x] == 3:
+                finishing_pos.append(y)
+                finishing_pos.append(x)
+    return finishing_pos
+
+# Convert a gene into a move array:
+def gene_to_move(GENE):
+
+    if GENE == '00': # Up
+        return [-1, 0]
+    elif GENE == '01': # Right
+        return [0, 1]
+    elif GENE == '10': # Down
+        return [1, 0]
+    elif GENE == '11': # Left
+        return [0, -1]
+
+# Check if the next move will make the agent out of bounds:
+def is_out_of_bounds(CURRENT_POSITION, MOVE_COORDS):
+    if CURRENT_POSITION[0] + MOVE_COORDS[0] <= 0:
+        return True
+    elif CURRENT_POSITION[1] + MOVE_COORDS[1] <= 0:
+        return True
+    print(CURRENT_POSITION[0] + MOVE_COORDS[0])
+    print(CURRENT_POSITION[1] + MOVE_COORDS[1])
+    return False
+
+# Check if the next move will hit an obstacle:
+def is_hitting_obstacle(CURRENT_POSITION, MOVE_COORDS, GRID_COORDS):
+    NEW_POS = [ CURRENT_POSITION[0] + MOVE_COORDS[0], CURRENT_POSITION[1] + MOVE_COORDS[1] ]
+    print("New Pos: ", NEW_POS)
+    if GRID_COORDS[NEW_POS[0]][NEW_POS[1]] == 1:
+        print(GRID_COORDS[NEW_POS[0]][NEW_POS[1]])
+        return True
+    return False
+
+
+
 def main():
 
+    # Initialise Window & Pygame:
     pygame.init()
     WINDOW_HEIGHT = 400
     WINDOW_WIDTH = 400
@@ -85,11 +152,26 @@ def main():
     GRID_HEIGHT = f.pop(0)
     GRID_VALUES = sort_grid_array(GRID_WIDTH, GRID_HEIGHT, f)
 
-    # Generate chromosomes:
+    # Generate first chromosome population:
     all_chromosomes = []
-    # for x in range(20):
-    #     all_chromosomes.append(gen_chromosome(5))
-    #     print(all_chromosomes[x])
+    for x in range(4):
+        all_chromosomes.append(gen_chromosome(8))
+        print(all_chromosomes[x])
+
+    # Find starting point coords on grid:
+    STARTING_POS = find_start_point(GRID_VALUES, GRID_HEIGHT, GRID_WIDTH)
+    END_POS = find_finish_point(GRID_VALUES, GRID_HEIGHT, GRID_WIDTH)
+
+    # Set the current coordinates of the chromosome:
+    CURRENT_POS = STARTING_POS
+    CURRENT_POS = [1, 2]
+    MOVE = gene_to_move('10')
+    print("Current Pos :", CURRENT_POS)
+    print("Move :", MOVE)
+    print(GRID_VALUES)
+    print("Is out of bounds?", is_out_of_bounds(CURRENT_POS, MOVE) )
+    print("Is hitting obstacle?",is_hitting_obstacle(CURRENT_POS, MOVE, GRID_VALUES))
+
 
     # Game Loop:
     while True:
