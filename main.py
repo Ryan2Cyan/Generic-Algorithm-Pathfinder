7be48be34ed634b698e4fba7e347e5f6591ab302
Cyan244466666 <cammellryan@gmail.com>
2021-12-10 16:13:18 +0000

    added a loop that can execute a chromosomes genes/moves

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ Gene Movement Correlation:
 11 - Left
 """
 
+
 # Init single bit (as a string):
 def gen_bit():
     return str(random.randint(0, 1))
@@ -33,7 +34,6 @@ def create_window(height, width):
 
 # Renders grid to screen:
 def draw_grid(WIDTH, HEIGHT, SCREEN, VALUES):
-
     blockSize = 40  # Set the size of the grid block
 
     rect_grid = []
@@ -48,8 +48,12 @@ def draw_grid(WIDTH, HEIGHT, SCREEN, VALUES):
     # Render Grid Squares
     for row in range(HEIGHT):
         for column in range(WIDTH):
-            pygame.draw.rect(SCREEN, grid_color(VALUES[row][column]), rect_grid[row][column], 0) # Grid Fill
-            pygame.draw.rect(SCREEN, (10, 30, 200), rect_grid[row][column], 1) # Grid Lines
+            pygame.draw.rect(SCREEN, grid_color(VALUES[row][column]), rect_grid[row][column], 0)  # Grid Fill
+            pygame.draw.rect(SCREEN, (10, 30, 200), rect_grid[row][column], 1)  # Grid Lines
+
+# Render grid the AI just moved to:
+def draw_move(SCREEN, MOVE):
+    return 0
 
 # Switch for Grid Square Color:
 def grid_color(grid_value):
@@ -104,50 +108,49 @@ def find_finish_point(GRID_ARRAY, HEIGHT, WIDTH):
 
 # Convert a gene into a move array:
 def gene_to_move(GENE):
-
-    if GENE == '00': # Up
+    if GENE == '00':  # Up
         return [-1, 0]
-    elif GENE == '01': # Right
+    elif GENE == '01':  # Right
         return [0, 1]
-    elif GENE == '10': # Down
+    elif GENE == '10':  # Down
         return [1, 0]
-    elif GENE == '11': # Left
+    elif GENE == '11':  # Left
         return [0, -1]
 
 # Check if the next move will make the agent out of bounds:
-def is_out_of_bounds(CURRENT_POSITION, MOVE_COORDS):
-    if CURRENT_POSITION[0] + MOVE_COORDS[0] <= 0:
+def is_out_of_bounds(CURRENT_POSITION, MOVE_COORDS, WIDTH, HEIGHT):
+    if CURRENT_POSITION[0] + MOVE_COORDS[0] < 0 or CURRENT_POSITION[0] + MOVE_COORDS[0] >= HEIGHT:
         return True
-    elif CURRENT_POSITION[1] + MOVE_COORDS[1] <= 0:
+    elif CURRENT_POSITION[1] + MOVE_COORDS[1] < 0 or CURRENT_POSITION[1] + MOVE_COORDS[1] >= WIDTH:
         return True
-    print(CURRENT_POSITION[0] + MOVE_COORDS[0])
-    print(CURRENT_POSITION[1] + MOVE_COORDS[1])
     return False
 
 # Check if the next move will hit an obstacle:
 def is_hitting_obstacle(CURRENT_POSITION, MOVE_COORDS, GRID_COORDS):
-    NEW_POS = [ CURRENT_POSITION[0] + MOVE_COORDS[0], CURRENT_POSITION[1] + MOVE_COORDS[1] ]
-    print("New Pos: ", NEW_POS)
+    NEW_POS = [CURRENT_POSITION[0] + MOVE_COORDS[0], CURRENT_POSITION[1] + MOVE_COORDS[1]]
     if GRID_COORDS[NEW_POS[0]][NEW_POS[1]] == 1:
-        print(GRID_COORDS[NEW_POS[0]][NEW_POS[1]])
+        return True
+    return False
+
+# Check if the next move will land on the end point:
+def is_hitting_end(CURRENT_POSITION, MOVE_COORDS, END_POINT):
+    NEW_POS = [CURRENT_POSITION[0] + MOVE_COORDS[0], CURRENT_POSITION[1] + MOVE_COORDS[1]]
+    if NEW_POS == END_POINT:
         return True
     return False
 
 
-
 def main():
-
     # Initialise Window & Pygame:
     pygame.init()
     WINDOW_HEIGHT = 400
     WINDOW_WIDTH = 400
     SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     SCREEN.fill((0, 0, 0))
-    CLOCK = pygame.time.Clock()
     pygame.display.set_caption("Genetic Algorithm Pathfinder")
 
     # Read Grid Text File:
-    f = extract_grid_from_file("MazeFilesForLab8\Lab8TerrainFile1.txt")
+    f = extract_grid_from_file("MazeFilesForLab8/Lab8TerrainFile1.txt")
     GRID_WIDTH = f.pop(0)
     GRID_HEIGHT = f.pop(0)
     GRID_VALUES = sort_grid_array(GRID_WIDTH, GRID_HEIGHT, f)
@@ -164,18 +167,35 @@ def main():
 
     # Set the current coordinates of the chromosome:
     CURRENT_POS = STARTING_POS
-    CURRENT_POS = [1, 2]
     MOVE = gene_to_move('10')
-    print("Current Pos :", CURRENT_POS)
     print("Move :", MOVE)
     print(GRID_VALUES)
-    print("Is out of bounds?", is_out_of_bounds(CURRENT_POS, MOVE) )
-    print("Is hitting obstacle?",is_hitting_obstacle(CURRENT_POS, MOVE, GRID_VALUES))
+    print("Is out of bounds?", is_out_of_bounds(CURRENT_POS, MOVE, GRID_WIDTH, GRID_HEIGHT))
+    print("Is hitting obstacle?", is_hitting_obstacle(CURRENT_POS, MOVE, GRID_VALUES))
+    print("Current Pos :", CURRENT_POS)
+    print("GRID_WIDTH:", GRID_WIDTH)
+    print("GRID_HEIGHT:", GRID_HEIGHT)
+    i = 0
 
+    # Pathfind for one chromosome:
+    for gene in all_chromosomes[0]:
+        CURRENT_MOVE = gene_to_move(gene)  # Generate next move array
+        print("Current Move:", CURRENT_MOVE)
+        if is_out_of_bounds(CURRENT_POS, CURRENT_MOVE, GRID_WIDTH, GRID_HEIGHT) == True:
+            print("Out of Bounds.")
+            continue
+        if is_hitting_obstacle(CURRENT_POS, CURRENT_MOVE, GRID_VALUES) == True:
+            print("Hit Obstacle.")
+            continue
+        else:
+            CURRENT_POS[0] = CURRENT_POS[0] + CURRENT_MOVE[0]
+            CURRENT_POS[1] = CURRENT_POS[1] + CURRENT_MOVE[1]
+            print("Moved to: ", CURRENT_POS)
 
     # Game Loop:
     while True:
         draw_grid(GRID_WIDTH, GRID_HEIGHT, SCREEN, GRID_VALUES)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -183,4 +203,5 @@ def main():
         pygame.display.update()
 
 
-main()
+if __name__ == "__main__":
+    main()
